5dec21df61f9dfeaea5ba86e6ab74af3d4d8f16c
Le Hai Quy Bui <Lbui1781@conestogac.on.ca>
2026-01-01 22:53:09 -0500

    Add Service Which Access To DB by predefine coded procedure

---
+++ SSRSite/D2LExtensionWebAPPSSR/Service/ICourseOperations.cs
@@ -0,0 +1,11 @@
+﻿namespace D2LExtensionWebAPPSSR.Service
+{
+    public interface ICourseOperations
+    {
+       
+        void CreateCourse(string UserId, string title, string description, string semester, string professor, string coursecode);
+        List<string> GetCoursesByUser(string UserId)
+        void UpdateCourse(int CourseId, string title, string description, string semester, string professor, string coursecode);
+        void DeleteCourse(int CourseId);
+    }
+}
